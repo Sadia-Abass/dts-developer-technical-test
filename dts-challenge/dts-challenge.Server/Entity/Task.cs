aa7eee8153e46af0ce043e591e23b234ccfada45
Sadia Abass <sadia.abass@outlook.com>
2025-04-14 19:10:37 +0100

    Add task entity class

---
+++ dts-challenge/dts-challenge.Server/Entity/Task.cs
@@ -0,0 +1,15 @@
+﻿namespace dts_challenge.Server.Entity
+{
+    public class Task
+    {
+        public long Id { get; set; }
+        public string? title { get; set; }
+        public string? description { get; set; }
+        public string? Stautus { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime DateTime { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
