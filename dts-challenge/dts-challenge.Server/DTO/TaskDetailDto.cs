62cdc6bc8c0b6623391a308d6c9c2321314846dd
Sadia Abass <sadia.abass@outlook.com>
2025-04-16 14:27:45 +0100

    Add dto class for task details

---
+++ dts-challenge/dts-challenge.Server/DTO/TaskDetailDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dts_challenge.Server.DTO
+{
+    public class TaskDetailDto
+    {
+        public long Id { get; set; }
+        public string? title { get; set; }
+        public string? description { get; set; }
+        public string? Stautus { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
