05b8502a5d05978d1d21ed65f269da89340a8f87
Sadia Abass <sadia.abass@outlook.com>
2025-04-17 13:04:25 +0100

    Add db context to the project

---
+++ dts-challenge/dts-challenge.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using dts_challenge.Server.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace dts_challenge.Server.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<CaseworkTask> CaseworkTasks { get; set; }
+    }
+}
