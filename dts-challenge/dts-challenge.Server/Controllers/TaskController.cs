c5e1318400ea6a79aab7540f9ae7bbf281244100
Sadia Abass <sadia.abass@outlook.com>
2025-04-17 23:57:55 +0100

    Add task controller

---
+++ dts-challenge/dts-challenge.Server/Controllers/TaskController.cs
@@ -0,0 +1,21 @@
+﻿using dts_challenge.Server.Data;
+using dts_challenge.Server.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dts_challenge.Server.Controllers
+{
+    [Route("api/task")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ApplicationDbContext _appDbContext;
+        private readonly ITaskRepository _taskRepository;
+
+        public TaskController(ApplicationDbContext appDbContext, ITaskRepository taskRepository)
+        {
+            _appDbContext = appDbContext ?? throw new ArgumentNullException(nameof(appDbContext));
+            _taskRepository = taskRepository ?? throw new ArgumentNullException(nameof(taskRepository));
+        }
+    }
+}
